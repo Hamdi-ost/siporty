b70c80ba14afd7839d86ad798605eb31141ca90a
HAMDI <benyaflahhamdi2@gmail.com>
2019-04-20 11:02:44 +0100

    payout method design

--- Front-End/Back-office/src/app/components/informations/informations.component.ts
+++ Front-End/Back-office/src/app/components/informations/informations.component.ts
@@ -6,12 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./informations.component.css']
 })
 export class InformationsComponent implements OnInit {
-  title = "Informations";
+  title = 'Informations';
   infos = [
-    { id: 1, username: 'hamdi', email: 'hamdi@gmail.com', card: '13121596', "income before": '12DT', "income after": '10DT' },
-    { id: 2, username: 'hamza', email: 'hamza@gmail.com', card: '13121596', "income before": '10DT', "income after": '5DT' }
+    { id: 1, 'name account': 'hamdi', bank: 'UIB', agency: 'Tunis', rib: '13121596', amount: '10DT' },
+    { id: 2, 'name account': 'hamza', bank: 'UIB', agency: 'Ariana', rib: '13121596', amount: '5DT' }
   ];
-  columnsName = ['username', 'email', 'card', 'income before', 'income after'];
+  columnsName = ['name account', 'bank', 'agency', 'rib', 'amount'];
 
   constructor() { }
 
--- Front-End/Client/src/app/components/profile/profile.component.ts
+++ Front-End/Client/src/app/components/profile/profile.component.ts
@@ -3,17 +3,27 @@ import { Subscription } from 'rxjs';
 import { first } from 'rxjs/operators';
 
 import { User } from '../../models';
-import { UserService, AuthenticationService } from '../../services';
+import { UserService, AuthenticationService, AlertService } from '../../services';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({ selector: 'app-profile', templateUrl: 'profile.component.html' })
 export class ProfileComponent implements OnInit, OnDestroy {
     currentUser: User;
     currentUserSubscription: Subscription;
     users: User[] = [];
+    loginForm: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
 
     constructor(
+        private userService: UserService,
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
         private authenticationService: AuthenticationService,
-        private userService: UserService
+        private alertService: AlertService
     ) {
         this.currentUserSubscription = this.authenticationService.currentUser.subscribe(user => {
             if (user) {
@@ -22,12 +32,51 @@ export class ProfileComponent implements OnInit, OnDestroy {
         });
     }
 
-    ngOnInit() {
-    }
-
     ngOnDestroy() {
         // unsubscribe to ensure no memory leaks
         this.currentUserSubscription.unsubscribe();
+    }
+
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            accountName: ['', Validators.required],
+            rib: ['', Validators.required],
+            bankName: ['', Validators.required],
+            agency: ['', Validators.required]
+        });
+
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    // convenience getter for easy access to form fields
+    get f() {
+        return this.loginForm.controls;
+    }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.loginForm.invalid) {
+            return;
+        }
+
+        console.log(this.loginForm.value);
+
+        // this.loading = true;
+        // this.authenticationService
+        //     .login(this.f.username.value, this.f.password.value)
+        //     .subscribe(
+        //         data => {
+        //             this.router.navigate([this.returnUrl]);
+        //             window.location.reload();
+        //         },
+        //         error => {
+        //             this.alertService.error(error);
+        //             this.loading = false;
+        //         }
+        //     );
     }
 
 
