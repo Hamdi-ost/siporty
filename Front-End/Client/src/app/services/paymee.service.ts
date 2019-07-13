a5e2906aa7de12b42c2e83df15eb8956f662c12c
Hamza Chebil <hamzachebil40@gmail.com>
2019-07-13 02:26:25 +0100

    paymee service

---
+++ Front-End/Client/src/app/services/paymee.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymeeService {
+
+  apiUrl = 'http://sandbox.paymee.tn';
+  token;
+  headers;
+
+  constructor(private http: HttpClient) { }
+
+  initiate(input) {
+    this.headers = new HttpHeaders().set('Authorization', 'Token eca19723415deb2806c333eb34b2b082768874e3');
+    return this.http.post(this.apiUrl + '/api/OPRequest/', input, { headers: this.headers })
+  }
+
+  startPayment(value) {
+    this.headers = new HttpHeaders().set('Authorization', 'Token eca19723415deb2806c333eb34b2b082768874e3');
+    return this.http.post(this.apiUrl + '/gateway/', value, { headers: this.headers })
+  }
+
+  verifyPayment(token) {
+    this.headers = new HttpHeaders().set('Authorization', 'Token eca19723415deb2806c333eb34b2b082768874e3');
+    return this.http.post(this.apiUrl + '/api/OPCheck/', token, { headers: this.headers });
+  }
+}
