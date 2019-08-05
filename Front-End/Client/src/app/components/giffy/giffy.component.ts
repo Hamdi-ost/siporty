660e399d49864f5e1428a197e79cb2e0d121ed69
Akrem Khezami <akrem.khezami96@gmail.com>
2019-08-05 11:02:18 +0100

    letspush

--- Front-End/Client/src/app/components/giffy/giffy.component.ts
+++ Front-End/Client/src/app/components/giffy/giffy.component.ts
@@ -7,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GiffyComponent implements OnInit {
 
+
+  counter;
+  intervalId;
   constructor() { }
 
   ngOnInit() {
-    $('body').css('background-color', 'rgb(0, 154, 64)');
+    $('body').css('background-color', 'rgb(255, 255, 255)');
+    document.getElementById('photo_equipe').style.display = 'none';
+    this.counter = 5 ;
+    this.intervalId = null;
+
+
   }
 
 }
